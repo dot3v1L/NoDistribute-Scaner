c719344aa635e37eb738ad88bc00fbf14dc389a6
dot3v1L <30367966+dot3v1L@users.noreply.github.com>
2018-03-02 16:44:29 +0300

    NoDistribute Scaner

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace NoDistribute_Scaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.tFile = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tProgres = new System.Windows.Forms.ToolStripProgressBar();
+            this.tError = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(287, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tFile
+            // 
+            this.tFile.Enabled = false;
+            this.tFile.Location = new System.Drawing.Point(7, 19);
+            this.tFile.Name = "tFile";
+            this.tFile.Size = new System.Drawing.Size(274, 20);
+            this.tFile.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(7, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(346, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tFile);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(5, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(359, 79);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Main";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tStatus,
+            this.tProgres,
+            this.tError});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 89);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(376, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tStatus
+            // 
+            this.tStatus.Name = "tStatus";
+            this.tStatus.Size = new System.Drawing.Size(124, 17);
+            this.tStatus.Text = "Status Uploading [0%]";
+            // 
+            // tProgres
+            // 
+            this.tProgres.Name = "tProgres";
+            this.tProgres.Size = new System.Drawing.Size(100, 16);
+            // 
+            // tError
+            // 
+            this.tError.Name = "tError";
+            this.tError.Size = new System.Drawing.Size(43, 17);
+            this.tError.Text = "Error []";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 111);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "NoDistribute Scaner";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tFile;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tStatus;
+        private System.Windows.Forms.ToolStripProgressBar tProgres;
+        private System.Windows.Forms.ToolStripStatusLabel tError;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace NoDistribute_Scaner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tStatus.ForeColor = Color.Black;
+            tError.ForeColor = Color.Black;
+            using (var open = new OpenFileDialog())
+            {
+                open.Title = "Select file";
+                open.Filter = ".exe|*.exe";
+
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    tFile.Text = open.FileName;
+                    using (WebClient webClient = new WebClient())
+                    {
+                        webClient.UploadProgressChanged += new UploadProgressChangedEventHandler(Upload);
+                        webClient.UploadFileCompleted += new UploadFileCompletedEventHandler(Complete);
+                        webClient.UploadFileAsync(new Uri("http://nodistribute.com/scan/index.php"), open.FileName);
+                    }
+                }
+            }
+        }
+
+        private void Complete(object sender, UploadFileCompletedEventArgs e)
+        {
+            try
+            {
+                string str = Encoding.UTF8.GetString(e.Result);
+                
+                if (str.Contains("You have reached the scan limit, try again in a few hours or"))
+                {
+                    Text = "NoDistribute Scaner [Limit scan]";
+                    tError.Text = "Error [Limit scan]";
+                    tError.ForeColor = Color.Red;
+                }
+
+                string[] res = GetResult(str);
+                if (res[0] != "")
+                {
+                    Text = String.Format("NoDistribute Scaner [{0}]", res[1]);
+                    textBox2.Text = res[0];
+                }
+                
+                tError.Text = String.Format("Error [{0}]", e.Error.Message);
+            }
+            catch (Exception){}
+        }
+
+        private void Upload(object sender, UploadProgressChangedEventArgs e)
+        {
+            try
+            {
+                tStatus.Text = String.Format("Status Uploading [{0}%]", e.ProgressPercentage);
+                tProgres.Value = e.ProgressPercentage;
+                tStatus.ForeColor = Color.Green;
+            }
+            catch (Exception){}
+        }
+
+        private string[] GetResult(string str)
+        {
+            string [] pattern = {"(?<=Verified By NoDistribute: ).*", "(?<=Detected by: ).*"};
+            string [] result = new string[2];
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                Regex regex = new Regex(pattern[i]);
+                Match match = regex.Match(str);
+                result[i] = match.Value;
+            }
+            return result;
+        }
+    }
+}
